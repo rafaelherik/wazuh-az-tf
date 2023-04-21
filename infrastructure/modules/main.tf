8eaf8e03ef11a9295f64c263871138784bcb2e0c
Rafael Herik <rafaelherik@gmail.com>
2023-04-21 13:53:18 +0200

    Add some configurations

--- infrastructure/modules/main.tf
+++ infrastructure/modules/main.tf
@@ -39,7 +39,28 @@ resource "azurerm_subnet" "example" {
 
 # KEYVAULT
 
+resource "azurerm_key_vault" "keyvault" {
+  name                =  var.keyvault.name
+  location            = "${azurerm_resource_group.wazuh-rg.location}"
+  resource_group_name = "${azurerm_resource_group.wazuh-rg.name}"
 
+  sku_name = "standard"
+  
+  tenant_id            = "${data.azurerm_client_config.current.tenant_id}"
+  enabled_for_disk_encryption = true
+
+  ## Setting the permissions to the service principal
+  access_policy {
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+    object_id = "${data.azurerm_client_config.current.object_id}"
+    key_permissions = [
+      "get",
+      "create",
+      "delete",
+      "list"
+    ]
+  }
+}
 
 
 # VIRTUAL MACHINES
--- infrastructure/workload/wazuh-infra.tfvars
+++ infrastructure/workload/wazuh-infra.tfvars
@@ -11,14 +11,14 @@ vnet = {
 
 wazuh-server = {
   name           = "my-wazuh-server-001"
-  vm_size        = "D2s_V2"
+  vm_size        = "Standard_DS2_v2"
   admin_username = "wazuhadmin"
 }
 
 
 wazuh-indexer = {
   name           = "my-wazuh-indexer-001"
-  vm_size        = "D2s_V2"
+  vm_size        = "Standard_DS2_v2"
   admin_username = "wazuhadmin"
 }
 
