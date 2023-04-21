8fb080992200ce669b5838e6fee236e9de42af41
Rafael Herik <rafaelherik@gmail.com>
2023-04-21 13:32:55 +0200

    update pipeline and tf configuration

--- infrastructure/modules/main.tf
+++ infrastructure/modules/main.tf
@@ -5,8 +5,7 @@ terraform {
 provider "azurerm" {
   subscription_id = var.azurerm_subscription_id
   tenant_id       = var.azurerm_tenant_id
-  client_id       = var.azurerm_client_id
-  client_secret   = var.azurerm_client_secret
+  client_id       = var.azurerm_client_id  
   features {}
 }
 
